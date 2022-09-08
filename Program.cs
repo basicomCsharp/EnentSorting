8dd1dc35d11c70b7a2ff51295c7adfe4db9e507a
basicomCsharp <coolandrey@gmail.com>
2022-09-08 21:09:18 +0500

    Модуль 9. Итоги. Задание 2.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ ReadConsol.ReadFromConsol();
 rc.NumberEnterEvent += Sorting;
 rc.InputNumberFromSort();
 Console.ReadKey();
-
+/// <summary>
+/// Метод определения и вызова типа сортировки
+/// </summary>
 static void Sorting(int number)
 {
     switch(number)
@@ -19,14 +21,17 @@ static void Sorting(int number)
             break;
     }
 }
-
+/// <summary>
+/// Класс ввода и сортировки фамилий
+/// </summary>
 public  class ReadConsol
-{
-    //public delegate void SortFamilyDelegate(List<String> fs);
+{    
     public delegate void NumberEnterDelegate(int i);
     public event NumberEnterDelegate NumberEnterEvent;
     public static List<String> Family = new List<String>();
-
+    /// <summary>
+    /// Метод ввода фамилий для коллекции
+    /// </summary>
     public static void ReadFromConsol()
     {
         try
@@ -43,8 +48,10 @@ public  class ReadConsol
         {
             Console.WriteLine(e.Message);
         }
-
     }
+    /// <summary>
+    /// Метод для ввода типа сортировки
+    /// </summary>
     public void InputNumberFromSort()
     {
         try
@@ -53,16 +60,23 @@ public  class ReadConsol
             var FlagSort = Convert.ToInt16( Console.ReadLine());
             EventRun(FlagSort);
         }
-        catch
+        catch (Exception e)
         {
+            Console.WriteLine(e.Message);
         }
     }
+    /// <summary>
+    /// вызов события
+    /// </summary>
+    /// <param name="num"></param>
     protected virtual void EventRun(int num)
     {
         NumberEnterEvent?.Invoke(num);
     }
 
-
+    /// <summary>
+    /// Метод сортировки коллекции в обратном порядке
+    /// </summary>
     public static void OmegaSort()
     {
         Family.Sort();
@@ -73,6 +87,9 @@ public  class ReadConsol
             Console.WriteLine(f);
         }
     }
+    /// <summary>
+    /// Метод сортировки коллекции по алфавиту
+    /// </summary>
     public static void AlfaSort()
     {
         Family.Sort();
